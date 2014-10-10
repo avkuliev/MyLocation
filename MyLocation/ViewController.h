0cd0798470b2098fdf5030d762d360ed432f1a0a
Alexander Kuliev <avkuliev@gmail.com>
2014-10-10 23:33:04 +0400

    Initial Commit

---
+++ MyLocation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyLocation
+//
+//  Created by Alexander Kuliev on 10/10/14.
+//  Copyright (c) 2014 Alexander Kuliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
